34fcf7abf8ebf427d6a941ea1f3b9ca53376d94b
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 19:28:52 +0700

    1. y = x2

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -1,18 +1,17 @@
-﻿
-using System;
+﻿using System;
 public class MathTest
 {
     static void Main(string[] args)
     {
-        for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
+        for (float i = -6; i < Math.PI * 2.0F; i += 0.3F)
         {
-            Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" +
-            spaces(Math.Sin(i)) + "*", i, Math.Sin(i));
+            Console.WriteLine("The y = x^2 of {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Pow(i, 2)) + "*", i, Math.Pow(i, 2));
         }
     }
     private static string spaces(double val)
     {
-        string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+        string SpaceString = new String(' ', ((int)(val * 2.0)) + 10);
         return SpaceString;
     }
 }
