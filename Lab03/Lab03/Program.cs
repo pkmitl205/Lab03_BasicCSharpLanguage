f3aed1b26649c4955fc214d8ead9205fb157c6dc
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 18:34:18 +0700

    การรับ string จากคีย์บอร์ด (2)

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -6,10 +6,10 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            char ch;
-            Console.Write("Press a key followed by ENTER:");
-            ch = (char)Console.Read();  // get a char
-            Console.WriteLine("Your key is: " + ch);
+            string str;
+            Console.Write("Enter some characters.");
+            str = Console.ReadLine();
+            Console.WriteLine("You entered: " + str);
         }
     }
 }
