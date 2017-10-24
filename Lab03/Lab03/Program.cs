32ec99856eff329712e6ee18be54989d62bc1e07
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 19:24:29 +0700

    หาค่าจากสมการทางคณิตศาสตร์

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -1,16 +1,19 @@
-﻿
-
-using System;
+﻿using System;
 public class intergerTest
 {
     static void Main(string[] args)
     {
-        int a, b, c, d, e, f;
-        a = 1;
-        b = a + 6;
-        c = b - 3;
-        d = c * 2;
-        e = d / 2;
-        f = e % 2;
+        float a = 10, b = 20, x = 5, y = 2;
+
+        Console.WriteLine("a+b = {0}", a + b);
+        Console.WriteLine("x-b = {0}", x - b);
+        Console.WriteLine("x*b = {0}", x * b);
+        Console.WriteLine("y/a = {0}", y / a);
+        Console.WriteLine("b%y = {0}", b % y);
+        Console.WriteLine("y+10%x = {0}", y + 10 % x);
+        Console.WriteLine("a/3*5 = {0}", a / 3 * 5);
+        Console.WriteLine("9/2*a = {0}", 9 / 2 * a);
+        Console.WriteLine("y%8 = {0}", y % 8);
+        Console.WriteLine("100*x+y%2-a = {0}", 100 * x + y % 2 - a);
     }
 }
