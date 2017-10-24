f628d6e75dcaaf3f0285cacf19b949017f63d285
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 19:28:09 +0700

    โปรแกรมพล็อตรูป sine wave บนหน้าจอ

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -1,13 +1,18 @@
-﻿using System;
-
-namespace Lab03
+﻿
+using System;
+public class MathTest
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Sin(i)) + "*", i, Math.Sin(i));
         }
+    }
+    private static string spaces(double val)
+    {
+        string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+        return SpaceString;
     }
 }
