1293b161ddf85673fae6367d3e3fb5694526c5fb
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 19:30:13 +0700

    2. y = cos(x)

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -3,15 +3,15 @@ public class MathTest
 {
     static void Main(string[] args)
     {
-        for (float i = -6; i < Math.PI * 2.0F; i += 0.3F)
+        for (float i = 1; i < Math.PI * 3.0F; i += 0.3F)
         {
-            Console.WriteLine("The y = x^2 of {0,10:F} = {1,-10:F6}" +
-            spaces(Math.Pow(i, 2)) + "*", i, Math.Pow(i, 2));
+            Console.WriteLine("The cos of {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
         }
     }
     private static string spaces(double val)
     {
-        string SpaceString = new String(' ', ((int)(val * 2.0)) + 10);
+        string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
         return SpaceString;
     }
 }
