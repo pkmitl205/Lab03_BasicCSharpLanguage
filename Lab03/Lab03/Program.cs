2fb6c7d06742577a74fe24b6f1868cf8b33b43c0
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 18:52:57 +0700

    แบบฝึกหัด

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -1,24 +1,24 @@
 ﻿using System;
-
-namespace Lab03
+namespace LAB03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.Write("Please enter value 1 :");
-                int val1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter value 2 :");
-                int val2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Answer = " + (val1 + val2));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error : " + e.ToString());
-            }
-
+            string name, lastname;
+            Console.Write("Please enter Name : ");
+            name = Console.ReadLine();
+            Console.Write("Please enter Lastname : ");
+            lastname = Console.ReadLine();
+            Console.Write("Please enter ID : ");
+            double var1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter GPA : ");
+            double var2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("*********************");
+            Console.WriteLine("Name : {0}", name);
+            Console.WriteLine("Lastname : {0}", lastname);
+            Console.WriteLine("ID : {0}", var1);
+            Console.WriteLine("GPA : {0}", var2);
         }
     }
 }
