e7d96fca7ba13a96211c4bb5c694233ecbcdced5
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 18:24:04 +0700

    การกำหนดรูปแบบของอาร์กิวเมนต์ (1)

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -6,7 +6,13 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            
+            int n = 123456789;
+            Console.WriteLine("{0, 0:E}", n);
+            Console.WriteLine("{0, 0:F}", n);
+            Console.WriteLine("{0, 0:G}", n);
+            Console.WriteLine("{0, 0:N}", n);
+            Console.WriteLine("{0, 0:P}", n);
+            Console.WriteLine("{0, 0:X}", n);
         }
     }
 }
