46be6ff73bfca34e3c73b6b618ec59e0a129538d
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 19:10:57 +0700

    การใช้งานข้อมูลชนิด boolean (1)

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
-
-namespace Lab03
+﻿
+using System;
+namespace Lab3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
+            Console.Write("Please enter Number : ");
+            int var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number : {0}", var1);
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+            Console.WriteLine("RandomNumber : {0}", randomNumber);
+
+            bool a = (var1 < randomNumber);
+            Console.WriteLine("{1} < {2} = {0}", a, var1, randomNumber);
         }
     }
 }
