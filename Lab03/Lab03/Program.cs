4cefde0107af1a8f891f62967c6177de02bb85e6
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 18:26:57 +0700

    การกำหนดรูปแบบพร้อมความกว้างของทศนิยมของอาร์กิวเมนต์ (1)

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -6,7 +6,12 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
         }
     }
 }
