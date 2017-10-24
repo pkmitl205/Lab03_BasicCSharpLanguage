71d8d3433ee946c9618f4d24243a4087d5eea053
pkmitl205 <58030205@kmitl.ac.th>
2017-10-24 19:03:40 +0700

    โปรแกรมแสดงคุณสมบัติ size, MinValue และ MaxValue ของชนิดข้อมูล

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -6,7 +6,10 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Data type : int");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
     }
 }
